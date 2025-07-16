b4715b66f38a4762073e62f15490b8100fa8c107
JDennisUF <jdennisuf@gmail.com>
2025-07-16 10:31:12 -0400

    Rebuild the Flutter web app and commit changes

--- flashcards/flutter_service_worker.js
+++ flashcards/flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"index.html": "40ae7d85d2d0176a90838d5ba96a3abe",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "651ef5ddc320ede5151ae05c17ff8dc7",
+"flutter_bootstrap.js": "bfc82ccf95016f2ed28c0b3f9e7372d8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c"};
 // The application shell files that are downloaded before a service worker can
 // start.
