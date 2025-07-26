6d1f8871b885f2d4c1bad9eacc5310e16b3d9b50
JDennisUF <jdennisuf@gmail.com>
2025-07-26 16:56:19 -0400

    Flashcards tweaks to show cards reversed

--- flashcards/flutter_bootstrap.js
+++ flashcards/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2886527861"
+    serviceWorkerVersion: "1953091607"
   }
 });
